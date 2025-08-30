a8c5b2ef18af8903173d991105bff5314731ae3d
DevBryan <briansoberonq@gmail.com>
2025-08-30 11:42:09 -0500

    connect to db, saving data of users

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from "../models/user.model.js";
+
+
+export const register = async (req, res) => {
+    const {email, password, username} = req.body
+    
+    try{
+        const newUser = new User({
+            username,
+            email,
+            password
+        })
+        console.log(newUser)
+        await newUser.save()
+        res.send("registrando")
+    }
+    catch(error){
+        console.log(error) 
+    }
+    
+    
+};
+
+export const login = (req, res) => res.send("login");
