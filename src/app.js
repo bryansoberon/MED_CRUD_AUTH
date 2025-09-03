8e3c979ee408a3de3f0b6dd50c42db74d7a3c1b0
DevBryan <briansoberonq@gmail.com>
2025-09-02 23:42:13 -0500

    user register with axios and cors. front send data to back

--- src/app.js
+++ src/app.js
@@ -4,10 +4,17 @@ import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/tasks.routes.js';
+import cors from 'cors';
 
 
 const app = express();
 
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
