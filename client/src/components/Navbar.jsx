bbe7d448daa04ea15547bb71d637efec5ce3757d
DevBryan <briansoberonq@gmail.com>
2025-09-11 11:02:21 -0500

    update navbar2

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,23 +12,36 @@ function Navbar() {
             <ul className="flex gap-x-2">
                 {isAuthenticated ? (
                     <>
-                    <li>Welcome {user.username}</li>
+                    <li>Welcome {user?.username}</li>
                     <li> 
                         <Link to="/add-task"  className='bg-indigo-500 px-4 py-2 rounded-sm'> 
                         New Task
                         </Link> 
                     </li> 
                     <li>
-                        <Link to="/" onClick={logout}>Logout</Link>
+                        <Link to="/" 
+                        onClick = {() =>{
+                            logout();
+                        }}
+                        >
+                        Logout 
+                        </Link>
                     </li> 
                     </>
                 ):(
                 <>
                 <li>
-                    <Link to="/login" className='bg-indigo-500 px-4 py-2 rounded-sm'> Login </Link> 
+                    <Link to="/login" className='bg-indigo-500 px-4 py-2 rounded-sm'> 
+                    Login 
+                    </Link> 
                 </li> 
                 <li>    
-                    <Link to="/register" className='bg-indigo-500 px-4 py-2 rounded-sm'> Register </Link>                </li> 
+                    <Link to="/register" 
+                    className='bg-indigo-500 px-4 py-2 rounded-sm'
+                    > 
+                        Register 
+                    </Link>        
+                </li> 
                 </>
             )}
             </ul>
