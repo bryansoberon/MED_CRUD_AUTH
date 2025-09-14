693927abc0bd558342d66a1ba6476b5cc0cf265a
DevBryan <briansoberonq@gmail.com>
2025-09-14 18:11:54 -0500

    personalized home

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,21 +4,17 @@ function HomePage() {
   return (
   <section className="bg-red-500 flex justify-center items-center">
     <header className="bg-zinc-800 p-10">
-      <h1 className="text-5xl py-2 font-bold">React Tasks</h1>
+      <h1 className="text-5xl py-2 font-bold">Bienvenido</h1>
       <p className="text-md text-slate-400">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos
-        fugit doloremque molestias recusandae labore repellat amet dicta tempore
-        necessitatibus facilis repellendus voluptas ducimus maiores deserunt sed
-        quo ratione provident debitis aut, voluptatem aliquam iste blanditiis
-        ex? Voluptatibus, fuga quasi necessitatibus cumque optio error enim,
-        officia accusantium vitae doloremque, molestias modi.
+        Aqui podras organizar tus tareas de forma sencilla y eficiente con React, todo lo que siempre quisiste y necesitabas, lo tienes de manera inmediata
+        con nostros, tenga usted una grandiosa bienvenida, por favor registrate o inicia sesion, posteriormente presione el boton de abajo para ver tus tareas actuales a completar, EXITOS.
       </p>
 
       <Link
         className="bg-zinc-500 text-white px-4 py-2 rounded-md mt-4 inline-block"
-        to="/register"
+        to="/tasks"
       >
-        Get Started
+        Ver mis tareas
       </Link>
     </header>
   </section>
